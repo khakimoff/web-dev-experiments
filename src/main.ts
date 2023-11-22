041d41a91e2ee21a95a5af2f58d41f5f49b2852b
Barakiat <rekhadmob@gmail.com>
2023-11-22 13:29:29 +0600

    Added example TypeGuard

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,26 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+interface Success {
+  res: 'success';
+  data: {a: 1}
+}
+interface Fail {
+  res: 'error';
+  erroMessage: string
+}
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+type ResType = Fail | Success
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function isResSucces(res: ResType): res is Success {
+  if(res.res === "success") {
+    return true;
+  }
+  return false
+}
+
+function process(res: ResType) {
+  if(isResSucces(res)) {
+    res.data
+  } else {
+    res.erroMessage
+  }
+}
+
